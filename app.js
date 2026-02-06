62359c4dc5a50c6c3185ac266116e85cc031340f
JaiderJannis <33848475+JaiderJannis@users.noreply.github.com>
2026-02-06 12:59:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ async function enableNotifications(uid) {
 
             // STAP 1: Handmatig de Service Worker registreren
             // De punt-slash (./) zorgt dat hij in de huidige map zoekt, in plaats van de root van het domein
-            const registration = await navigator.serviceWorker.register('./firebase-messaging-sw.js');
+            const registration = await navigator.serviceWorker.register('/shift-planner/firebase-messaging-sw.js');
             
             // Haal het unieke token op
             const token = await getToken(messaging, {
