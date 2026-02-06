0f0524412e050bf0e4361d1ad56ad4f57b6a043a
JaiderJannis <33848475+JaiderJannis@users.noreply.github.com>
2026-02-06 12:31:32 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,16 +1,27 @@
-importScripts('https://www.gstatic.com/firebasejs/10.12.4/firebase-app-sw.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging-sw.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.4/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging-compat.js');
 
-firebase.initializeApp({
+// Gebruik exact dezelfde config als in je hoofdscript
+const firebaseConfig = {
   apiKey: "AIzaSyB8uHwRXCe1iV7z6T80YPxEbeB64qdMpNY",
+  authDomain: "shift-planner-dc7ad.firebaseapp.com",
   projectId: "shift-planner-dc7ad",
   messagingSenderId: "719441527396",
   appId: "1:719441527396:web:de87d6f950fe23702a5571"
 });
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Optioneel: Achtergrond berichten afhandelen
+// Luister naar berichten wanneer de app op de achtergrond draait
 messaging.onBackgroundMessage((payload) => {
-  console.log('Bericht ontvangen in achtergrond:', payload);
+  console.log('[sw.js] Bericht ontvangen: ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png' // Zorg dat dit bestand bestaat of pas het pad aan
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
